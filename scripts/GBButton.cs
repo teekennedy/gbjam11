d768f18d169086a5788fb91b46295b931ec58cba
Terrance Kennedy <terrance@missingtoken.net>
2023-09-17 11:59:07 -0600

    Add basic UI

---
+++ scripts/GBButton.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class GBButton : Button
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        var focusedIcon = GD.Load<CompressedTexture2D>("res://ui/focused.png");
+        FocusEntered += () => Icon = focusedIcon;
+        Pressed += () => Icon = focusedIcon;
+        FocusExited += () => Icon = null;
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+}
